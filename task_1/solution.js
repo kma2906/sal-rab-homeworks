de9e4352805c4dad015b64797e5b3761621afec8
Neto test <kma290694@gmail.com>
2023-04-14 17:26:38 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,19 +2,18 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
-    // Задание №1.1. Оповещение о добавлении в корзину
+    
 
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+    let message;
+    message = productName;
+    message = `${message} `;
+    message = `${message}за `;
+    message = message + productPrice;
+    message = `${message} `;
+    message = `${message}теперь в корзине!`
+    console.log (message)
 
-    // Конец решения задания №1.1.
+
 
     return message;
 }
@@ -24,11 +23,11 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+    let newValue;
+    newValue = oldValue;
+    newValue =(oldValue + 1);
 
-    // Конец решения задания №1.2.
+   
 
     return newValue;
 }
@@ -39,9 +38,10 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText = `${newSum}Р`;
+    
 
     // Конец решения задания №1.3.
 
